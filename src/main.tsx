7bbcfe9865ffd0d68380bdf0bead312b63ff9b7a
Josbor <josbor.dev@gmail.com>
2022-12-12 19:18:06 -0400

    new try

--- src/main.tsx
+++ src/main.tsx
@@ -4,6 +4,7 @@ import { BrowserRouter } from 'react-router-dom'
 import App from './App'
 import './index.scss'
 import 'normalize.css'
+import * as serviceWorkerRegistration from "./serviceWorkerRegistration";
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
@@ -13,3 +14,15 @@ ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
     
   </React.StrictMode>
 )
+serviceWorkerRegistration.register({
+  onUpdate: async (registration:any) => {
+    // Corremos este código si hay una nueva versión de nuestra app
+    // Detalles en: https://developers.google.com/web/fundamentals/primers/service-workers/lifecycle
+    if (registration && registration.waiting) {
+      await registration.unregister();
+      registration.waiting.postMessage({ type: "SKIP_WAITING" });
+      // Des-registramos el SW para recargar la página y obtener la nueva versión. Lo cual permite que el navegador descargue lo nuevo y que invalida la cache que tenía previamente.
+      window.location.reload();
+    }
+  },
+});
