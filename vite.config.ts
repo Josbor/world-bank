fefa01709378b879894a6f7f1d858aa9885aa535
Josbor <josbor.dev@gmail.com>
2022-12-12 18:58:49 -0400

    new  hope

--- vite.config.ts
+++ vite.config.ts
@@ -4,5 +4,41 @@ import { VitePWA } from 'vite-plugin-pwa'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(), VitePWA({ registerType: 'autoUpdate' })]
+  plugins: [ react(),   VitePWA({
+    registerType: 'autoUpdate',
+    devOptions: {
+      enabled: true
+    },
+    mode: "development",
+    base: "/",
+    srcDir: "src",
+    filename: "sw.ts",
+    includeAssets: ["/favicon.png"],
+    strategies: "injectManifest",
+    manifest: {
+      name: "PWA WORLD CASH",
+      short_name: "WC",
+      theme_color: "#6767e1",
+      start_url: "/",
+      display: "standalone",
+      background_color: "#ffffff",
+      icons: [
+        {
+          "src": "assets/react.svg",
+          "sizes": "any",
+          "purpose":"any"
+        }
+      
+     
+      ],
+      lang:"es-MX",
+      orientation:"portrait", 
+      prefer_related_applications:false,
+      scope: "/",
+
+
+    },
+  
+
+  })]
 })
